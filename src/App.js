9ee09fe33baff83030b9cde4f4ba36640dfe4698
zuzhus <106622750+zuzhus@users.noreply.github.com>
2023-08-26 14:09:01 +0200

    Added GitHub repository and footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">Weather App</div>
+    <div className="App">
+      Weather App
+   <footer>
+   This project wac coded by Zuzana Hušeková and {" "}  
+    <a href="https://github.com/zuzhus/react-weather-app" 
+    target="_blank"> open-sourced on GitHub </a></footer></div>
   );
 }
 
