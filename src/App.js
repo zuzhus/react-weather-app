35b9433b45125639f6402e669b95669f1431a92c
zuzhus <106622750+zuzhus@users.noreply.github.com>
2023-08-26 14:33:09 +0200

    correct blank target

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       Weather App
    <footer>
    This project wac coded by Zuzana Hušeková and {" "}  
-    <a href="https://github.com/zuzhus/react-weather-app" target="_blank"
+    <a href="https://github.com/zuzhus/react-weather-app" target="_blank" rel="noreferrer"
     > open-sourced on GitHub </a></footer></div>
   );
 }
