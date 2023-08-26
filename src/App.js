487b98f527991839f4abb92bdd71969beae92ff9
zuzhus <106622750+zuzhus@users.noreply.github.com>
2023-08-26 16:09:38 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       Weather App
    <footer>
    This project wac coded by Zuzana Hušeková and {" "}  
     <a href="https://github.com/zuzhus/react-weather-app" target="_blank" rel="noreferrer"
     > open-sourced on GitHub </a></footer></div>
-  );
+  </div>);
+  
 }
 
 
