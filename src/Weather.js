b0fadf0c58d68fd644ada64b1a88f8fa5e4db009
zuzhus <106622750+zuzhus@users.noreply.github.com>
2023-08-27 22:54:02 +0200

    added weather icons

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather(props) {
             city: response.data.name,
             date: new Date(response.data.dt * 1000),
             description: response.data.weather[0].description,
-            iconUrl:"https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+            icon:response.data.weather[0].icon,
             temperature: response.data.main.temp,
             humidity: response.data.main.humidity,
             wind: response.data.wind.speed,
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
 return ( 
@@ -13,14 +14,14 @@ return (
           <div className="row mt-3">
             <div className="col-8">
                 <div className="clearfix">
-                    <img src={props.data.iconUrl} 
-                    alt={props.data.description}
-                    className="float-left"/>
+                    <div className="float-left">
+                    <WeatherIcon code={props.data.icon} />
+                    
                     <span className="float-left"></span>
                     <span className="temperature">{Math.round(props.data.temperature)}</span>
                     <span className="unit">°C</span>
                 </div>
-                
+                </div>
                 </div> 
          <div className="col-4">
                 <ul>
