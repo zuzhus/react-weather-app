e5f883583efb9d5eea6a0ac18f4c5898d0eb77e6
zuzhus <106622750+zuzhus@users.noreply.github.com>
2023-08-29 19:15:13 +0200

    fixed loading coords in forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState , useEffect } from "react";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
@@ -11,6 +11,11 @@ function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
 }
+
+useEffect(() => {
+    setLoaded(false);
+}, [props.coordinates]);
+
     
 if (loaded) {
 
